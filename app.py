9e9b378caf7ff75a1014c19b40fe9d6ed1e5cd8e
Kyle Lawrence <kylelawrence@Aspens-MacBook-Air.local>
2024-03-19 13:47:07 -0700

    ingest expenses sheet from downloaded csv and transform to dataframe

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import shutil
 from PIL import Image
 import exif
 from pillow_heif import register_heif_opener
+import pandas as pd
 
 def list_files(directory:str) -> list:
     # get list of files in directory
@@ -26,15 +27,15 @@ def copy_files_to_dir(files:list, copy_from:str, copy_to:str):
 
 def convert_heic_to_jpg(heic_directory:str, heic_file_name:str,  save_directory:str, saved_file_prefix:str='from_heic_') -> None:
     # converts HEIC image to JPG
-    if file_name.endswith('.HEIC'):
+    if heic_file_name.endswith('.HEIC'):
         # convert an HEIC image to JPG
         register_heif_opener()
         image = Image.open(f'{heic_directory}/{heic_file_name}')
-        print(f"original orientation for {file_name}: {image.info['original_orientation']}")
+        print(f"original orientation for {heic_file_name}: {image.info['original_orientation']}")
         if image.info['original_orientation']==3:
             image = image.rotate(90, expand=True)
         else:
-            print(f'The orientation of {file_name} is not 3')
+            print(f'The orientation of {heic_file_name} is not 3')
         new_image_name = f"{save_directory}/{saved_file_prefix}{heic_file_name.replace('.HEIC','.jpg')}"
         image.save(new_image_name)
         print(f"image saved to {new_image_name}")
@@ -57,18 +58,38 @@ def get_exif_data(directory:str, file_name:str):
             transformed_image.write(my_image.get_file())
     else:
         raise RuntimeError('The file type must be JPG')
+    
+def read_expenses(file:str='Budget - Expenses.csv'):
+    df = pd.read_csv(file, usecols=[0,2,3,4,5,6])
+    new_col_names = {
+        'Purchase Date (mm-dd-yy)':'purchase_date'
+        ,'Item_lowercase':'item'
+        ,'Price':'price'
+        ,'Receipt Number':'receipt_number'
+        ,'Biz Name':'business_name'
+        ,'IS CAD':'is_cad'
+        }
+    df.rename(columns=new_col_names, inplace=True)
+    df['receipt_number'] = df['receipt_number'].astype("Int64")
+    df['purchase_date'] = pd.to_datetime(df['purchase_date'], format='%m/%d/%Y')
+    for col in ['item','business_name']:
+        df[col] = df[col].str.lower()
+    return df
 
 if __name__=='__main__':
     images_dir = '.images'
-    file_name = 'IMG_4582.HEIC'
     saved_file_prefix = 'from_heic_'
     processed_images_dir = '.processed_images'
-    
+
     list_images = list_files(images_dir)
     heics = [image for image in list_images if image.upper().endswith('HEIC')]
     jpgs = [image for image in list_images if image.upper().endswith('JPG')]
     
-    for image in heics:
-        convert_heic_to_jpg(images_dir, image, processed_images_dir)
+    #for image in heics:
+    #    convert_heic_to_jpg(images_dir, image, processed_images_dir)
 
-    copy_files_to_dir(jpgs, images_dir, processed_images_dir)
+    #copy_files_to_dir(jpgs, images_dir, processed_images_dir)
+
+    expenses = read_expenses()
+    print(expenses.info())
+    print(expenses.head())
