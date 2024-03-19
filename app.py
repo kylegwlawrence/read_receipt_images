57073491ed76b432a33c4b813a17e7c76188cdde
Kyle Lawrence <kylelawrence@Aspens-MacBook-Air.local>
2024-03-19 10:06:58 -0700

    convert an HEIC image from iCloud to a png image

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import os
+from PIL import Image
+from pillow_heif import register_heif_opener
 
 # define directory where images are stored
 images_dir = '.images'
@@ -8,7 +10,13 @@ images_dir = '.images'
 files = []
 # loop over DirEntry object with scandir
 for entry in os.scandir(images_dir):
-        if not entry.is_dir():
+        # filter out directories and duplicates
+        if not entry.is_dir() and not entry.name.endswith('copy.JPG'):
             # DirEntry method
             files.append(entry.name)
-print(files)
+
+# convert an HEIC image to png
+register_heif_opener()
+
+image = Image.open(f'{images_dir}/IMG_4511.HEIC')
+image.save('.processed_images/from_heic_IMG_4511.png', format('png'))
